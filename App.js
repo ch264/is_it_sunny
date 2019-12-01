65df513ebc46437b05a7199392ae8a36e6dbe845
ch264 <christinavhastenrath@gmail.com>
2019-11-30 17:28:36 -0800

    importing api key and updating state. fetch data and update state. using navigator api for current location, and pass lon and lat to custom function fetchWeather where api is called

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+
+import { API_KEY } from './utils/WeatherKeys';
+
+import Weather from './components/Weather';
 
 export default class App extends React.Component {
 
+  // store json data from api in state
   state = {
-    isLoading: false
+    isLoading: false,
+    temperature: 0;
+    weatherCondition: null,
+    error: null
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error, cannot look outside'
+        })
+      }
+    )
+  }
+
+  fetchWeather(lat = 25, long = 25) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    ).then (res => res.json()).then(json => {
+      console.log(json);
+    });
   }
 
   render() {
@@ -15,12 +44,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
       {isLoading ? (
         <text>Checking The Weather Stone</text>
-      ) : (
-        <View>
-          <Text style={styles.paragraph}>
-          weather in SF today</Text>
-          </View>
-      )}
+      ) : <Weather />}
       </View>
     );
   }
